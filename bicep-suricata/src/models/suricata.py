f962b21016f3b349c8d57c66333b13a8cf2e5314
maldwg <maxludwig3@t-online.de>
2024-05-30 15:27:26 +0200

    add env variables and minimal configuration implementation for suricata

--- bicep-suricata/src/models/suricata.py
+++ bicep-suricata/src/models/suricata.py
@@ -1,8 +1,14 @@
 from  src.utils.models.ids_base import IDSBase
+from fastapi import UploadFile
+import os
+import shutil
 
+# TODO: correct paths 
+# TODO: what to do about rules?
 class Suricata(IDSBase):
     configuration_location = "/tmp/suricata.yaml"
     ruleset_location = "/tmp/rules.yaml"
 
-    def configure(self):
-        return "needs to be implemented suricata"
+    def configure(self, temporary_file):
+        shutil.move(temporary_file, self.configuration_location)
+        return "succesfuly moved file"
