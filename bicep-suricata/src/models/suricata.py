cf585c139528be5752743ac71ff191c1b0b194fe
maldwg <maxludwig3@t-online.de>
2024-06-06 14:56:19 +0200

    begin work on static analysis

--- bicep-suricata/src/models/suricata.py
+++ bicep-suricata/src/models/suricata.py
@@ -1,13 +1,13 @@
 from  src.utils.models.ids_base import IDSBase
 from src.utils.fastapi.routes import tell_core_analysis_has_finished
 import shutil
+from ..utils.fastapi.utils import execute_command, wait_for_process_completion
 
-# TODO: correct paths 
-# TODO: what to do about rules?
 class Suricata(IDSBase):
     configuration_location: str = "/tmp/suricata.yaml"
     ruleset_location: str = "/tmp/rules.yaml"
     container_id: int = None
+    pid: int = None
 
     async def configure(self, file_path):
         shutil.move(file_path, self.configuration_location)
@@ -22,8 +22,18 @@ class Suricata(IDSBase):
     
     async def startStaticAnalysis(self, file_path, container_id):
         self.container_id = container_id
-        return "Started Static Analysis"
+        # TODO: add config as well + ruleset?
+        command = ["suricata", "-r", file_path]
+        pid = await execute_command(command)
+        self.pid = pid
+        return_code = await wait_for_process_completion(pid)
+        if return_code is not None:
+            return f"Process with PID {pid} has exited with return code {return_code}"
+        else:
+            return f"Process with PID {pid} does not exist"
     
     async def stopAnalysis(self):
+        # TODO: stop the process using the pid
         await tell_core_analysis_has_finished(container_id=self.container_id)
         return "Stopped analysis"
+
