15d482abc021bb4476c96811b718f5c60f45291d
maldwg <maxludwig3@t-online.de>
2024-06-13 11:27:21 +0200

    update suricata to be able to use the state mechanic

--- bicep-suricata/src/main.py
+++ bicep-suricata/src/main.py
@@ -1,11 +1,34 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from src.utils.fastapi import routes
+import os
+import importlib
+from .utils.models.ids_base import IDSBase
+
+
 app = FastAPI()
 
 origins = [
     "*"
 ]
+
+
+@app.on_event("startup")
+async def startup_event():
+    module_name = os.getenv("IDS_MODULE")
+    class_name = os.getenv("IDS_CLASS")
+
+    if not module_name or not class_name:
+        raise ValueError("IDS_MODULE or IDS_CLASS environment variable not set")
+
+    module = importlib.import_module(module_name)
+    cls = getattr(module, class_name)
+
+    if not issubclass(cls, IDSBase):
+        raise TypeError(f"{class_name} is not a subclass of IDSBase")
+
+    app.state.ids_instance = cls()
+
 
 app.add_middleware(
     CORSMiddleware,
--- bicep-suricata/src/models/suricata.py
+++ bicep-suricata/src/models/suricata.py
@@ -29,9 +29,7 @@ class Suricata(IDSBase):
         shutil.move(file_path, self.ruleset_location)
         return "succesfuly setup ruleset"
 
-    async def startNetworkAnalysis(self, container_id):
-        self.container_id = container_id
-
+    async def startNetworkAnalysis(self):
         # set network adapter to promiscuous mode
         command = ["ip", "link", "set", self.network_interface, "promisc", "on"]
         await execute_command(command)
@@ -39,12 +37,21 @@ class Suricata(IDSBase):
         command = ["suricata", "-c", self.configuration_location, "-i", self.network_interface, "-l", self.log_location]
         pid = await execute_command(command)
         self.pid = pid
-        return {"message": f"started network analysis for container with {container_id}"}
+        return {"message": f"started network analysis for container with {self.container_id}"}
     
-    async def startStaticAnalysis(self, file_path, container_id):
-        self.container_id = container_id
+    async def startStaticAnalysis(self, file_path):
         command = ["suricata", "-c", self.configuration_location, "-S", self.ruleset_location,  "-r", file_path, "-l", self.log_location]
         pid = await execute_command(command)
         self.pid = pid
         await wait_for_process_completion(pid)
         await self.stopAnalysis()            
+
+
+    # overrides the default method
+    async def stopAnalysis(self):
+        from src.utils.fastapi.utils import stop_process
+        from src.utils.fastapi.routes import tell_core_analysis_has_finished
+
+        await stop_process(self.pid)
+        self.pid = None
+        await tell_core_analysis_has_finished(self)
