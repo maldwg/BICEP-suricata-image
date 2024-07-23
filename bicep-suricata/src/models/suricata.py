ad611e718ae845d5bbb0b7a83da9ef0564725a78
maldwg <maxludwig3@t-online.de>
2024-07-23 14:01:48 +0200

    Begin adding parser for alerts

--- bicep-suricata/src/models/suricata.py
+++ bicep-suricata/src/models/suricata.py
@@ -1,19 +1,22 @@
+import asyncio
 from  src.utils.models.ids_base import IDSBase
-from src.utils.fastapi.routes import tell_core_analysis_has_finished
 import shutil
 import os
 from ..utils.fastapi.utils import execute_command, wait_for_process_completion, stop_process
+from .suricata_parser import SuricataParser
+from ..utils.models.ids_base import Alert
 
 class Suricata(IDSBase):
     configuration_location: str = "/tmp/suricata.yaml"
     log_location: str = "/opt/logs"
     pid: int = None
+    send_alerts_task = None
     # the interface to listen on in network analysis modes
     network_interface = "eth0"
 
     # unique variables
     ruleset_location: str = "/tmp/custom_rules.rules"
-
+    parser = SuricataParser()
 
     async def configure(self, file_path):
         shutil.move(file_path, self.configuration_location)
@@ -37,13 +40,19 @@ class Suricata(IDSBase):
         command = ["suricata", "-c", self.configuration_location, "-i", self.network_interface, "-l", self.log_location]
         pid = await execute_command(command)
         self.pid = pid
+
+        self.send_alerts_task = asyncio.create_task(self.parser.parse_alerts_from_network_traffic())
+
         return f"started network analysis for container with {self.container_id}"
     
     async def startStaticAnalysis(self, file_path):
+        from src.utils.fastapi.routes import send_alerts_to_core
         command = ["suricata", "-c", self.configuration_location, "-S", self.ruleset_location,  "-r", file_path, "-l", self.log_location]
         pid = await execute_command(command)
         self.pid = pid
         await wait_for_process_completion(pid)
+        alerts: list[Alert] = await self.parser.parse_alerts_from_file()
+        await send_alerts_to_core(ids=self, alerts=alerts, analysis_type="static")
         await self.stopAnalysis()            
 
 
@@ -53,5 +62,6 @@ class Suricata(IDSBase):
         from src.utils.fastapi.routes import tell_core_analysis_has_finished
 
         await stop_process(self.pid)
+        await self.send_alerts_task.cancel()
         self.pid = None
         await tell_core_analysis_has_finished(self)
---
+++ bicep-suricata/src/models/suricata_parser.py
@@ -0,0 +1,13 @@
+from src.utils.models.ids_base import IDSParser, Alert
+
+class SuricataParser(IDSParser):
+
+    alertFileLocation = ""
+
+    # TODO 4: parser for static traffic
+    async def parse_alerts_from_network_traffic():
+        pass
+
+    # TODO 4: parser for network traffic
+    async def parse_alerts_from_network_traffic():
+        pass
