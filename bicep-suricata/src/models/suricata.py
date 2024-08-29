4aaac083ec82159ebcbe137f2581c78002f0dea1
maldwg <maxludwig3@t-online.de>
2024-08-29 12:12:09 +0200

    adapted suricata to work with tapped network interfaces

--- bicep-suricata/src/models/suricata.py
+++ bicep-suricata/src/models/suricata.py
@@ -2,9 +2,9 @@ import asyncio
 from  src.utils.models.ids_base import IDSBase
 import shutil
 import os
-from ..utils.fastapi.utils import execute_command, wait_for_process_completion, stop_process, send_alerts_to_core, send_alerts_to_core_periodically
+from ..utils.general_utilities import create_and_activate_network_interface,remove_network_interface,mirror_network_traffic_to_interface,execute_command, wait_for_process_completion
+from ..utils.fastapi.utils import  send_alerts_to_core, send_alerts_to_core_periodically
 from .suricata_parser import SuricataParser
-from ..utils.models.ids_base import Alert
 
 class Suricata(IDSBase):
     configuration_location: str = "/tmp/suricata.yaml"
@@ -31,13 +31,13 @@ class Suricata(IDSBase):
         return "succesfuly setup ruleset"
 
     async def startNetworkAnalysis(self):
-        # set network adapter to promiscuous mode
-        command = ["ip", "link", "set", self.network_interface, "promisc", "on"]
-        await execute_command(command)
-
-        command = ["suricata", "-c", self.configuration_location, "-i", self.network_interface, "-S", self.ruleset_location, "-l", self.log_location]
-        pid = await execute_command(command)
-        self.pid = pid
+        self.tap_interface_name = f"tap{self.container_id}"
+        await create_and_activate_network_interface(self.tap_interface_name)
+        pid = await mirror_network_traffic_to_interface(default_interface="eth0", tap_interface=self.tap_interface_name)
+        self.pids.append(pid)
+        start_suricata = ["suricata", "-c", self.configuration_location, "-i", self.tap_interface_name, "-S", self.ruleset_location, "-l", self.log_location]
+        pid = await execute_command(start_suricata)
+        self.pids.append(pid)
 
         self.send_alerts_periodically_task = asyncio.create_task(send_alerts_to_core_periodically(ids=self))
 
@@ -46,9 +46,10 @@ class Suricata(IDSBase):
     async def startStaticAnalysis(self, file_path):
         command = ["suricata", "-c", self.configuration_location, "-S", self.ruleset_location,  "-r", file_path, "-l", self.log_location]
         pid = await execute_command(command)
-        self.pid = pid
+        self.pids.append(pid)
+
         await wait_for_process_completion(pid)
-        self.pid = None
+        self.pids.remove(pid)
         if self.static_analysis_running:
             await send_alerts_to_core(ids=self)
         await self.stopAnalysis()            
@@ -56,15 +57,15 @@ class Suricata(IDSBase):
 
     # overrides the default method
     async def stopAnalysis(self):
-        from src.utils.fastapi.utils import stop_process, tell_core_analysis_has_finished
+        from src.utils.fastapi.utils import tell_core_analysis_has_finished
         self.static_analysis_running = False
-        # when triggered after the static analysis , then this is already none and can be skipped
-        if self.pid != None:
-            await stop_process(self.pid)
-            self.pid = None
+        await self.stop_all_processes()
         if self.send_alerts_periodically_task != None:            
             print(self.send_alerts_periodically_task)
             if not self.send_alerts_periodically_task.done():
                 self.send_alerts_periodically_task.cancel()
             self.send_alerts_periodically_task = None
+        print(self.tap_interface_name)
+        if self.tap_interface_name != None:
+            await remove_network_interface(self.tap_interface_name)
         await tell_core_analysis_has_finished(self)
